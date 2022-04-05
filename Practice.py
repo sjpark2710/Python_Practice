4e248b48cbfeea2c5244a193d49ccc2de14f84d9
seongjin <tjdwns95^^>
2022-04-05 19:34:03 +0900

    innit

---
+++ Practice.py
@@ -0,0 +1,23 @@
+a = "hobby"
+a
+
+#문자 개수 세기count 함수
+print(a)
+a.count('b')
+print(a.count('b'))
+
+'''
+위치 알려주기 1 find 함수
+'''
+c = "Python is the best choice"
+a.find('c')
+print(c.find('c'))
+print(c.find('P'))
+
+#위치 알려주기 2 index 함수
+
+a = "Life is too short"
+print(a)
+a.index('i')
+print(a.index('i'), a.index('t'))
+
