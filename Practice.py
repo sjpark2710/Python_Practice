8bc9840a37f405de39689f9b76419dbb9db1c3a0
seongjin <tjdwns95^^>
2022-04-05 20:12:31 +0900

    innit

--- Practice.py
+++ Practice.py
@@ -2,6 +2,7 @@ a = "hobby"
 a
 
 #문자 개수 세기count 함수
+print("문자 개수 세기count 함수")
 print(a)
 a.count('b')
 print(a.count('b'))
@@ -9,15 +10,32 @@ print(a.count('b'))
 '''
 위치 알려주기 1 find 함수
 '''
+print("위치 알려주기 1 find 함수")
 c = "Python is the best choice"
 a.find('c')
 print(c.find('c'))
 print(c.find('P'))
 
 #위치 알려주기 2 index 함수
-
+print("위치 알려주기 2 index 함수")
 a = "Life is too short"
 print(a)
 a.index('i')
 print(a.index('i'), a.index('t'))
+
+#문자열 삽입 join 함수
+print("문자열 삽입 join 함수")
+",".join('abcd')
+
+print(",".join('abcd'))
+
+#소문자를 대문자로 바꾸기 upper 함수
+print("소문자를 대문자로 바꾸기 upper 함수")
+a = "hi"
+print(a)
+a.upper()
+print(a.upper())
+
+
+
 
