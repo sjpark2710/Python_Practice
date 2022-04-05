bde0df66ccc362931f79a66ab55cc86589103e11
seongjin <tjdwns95^^>
2022-04-05 22:14:37 +0900

    add

--- List.py
+++ List.py
@@ -2,3 +2,43 @@
 #리스트명 = [요소1, 요소2, 요소3....]
 odd = [1,3,5,7,9]
 print(odd)
+
+a = []
+b = [1,2,3]
+c = ['Life', 'is','too','short']
+d = [1,2,'Life','is']
+e = [1,2, ['Life', 'is']]
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+#리스트 인덱싱
+print("리스트 인덱싱")
+a = [1,2,3]
+print(a)
+
+print(a[0])
+print(a[0] + a[2])
+print(a[-1])
+
+a=[1,2,3,['a', 'b', 'c']]
+print(a)
+
+print(a[0])
+print(a[-1])
+print(a[3])
+
+print(a[-1][0])
+
+print(a[-1][2])
+
+a = [1,2, ['1', 'b', ["Life", "is"]]]
+print(a)
+print(a[2][0])
+print(a[2][2])
+print(a[2][2][1])
+
+#리스트 슬라이싱
