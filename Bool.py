75b708bdfdc85e1873cbeb67f4748d4804c4ab8d
seongjin <tjdwns95^^>
2022-04-17 17:41:21 +0900

    add1

---
+++ Bool.py
@@ -0,0 +1,37 @@
+
+print("불 자료형")
+print("불이란 참과 거짓을 나타내는 자료형")
+
+a = True
+b = False
+
+type(a)
+print(type(a))
+
+print("1==1",1==1)
+print("2>1",2>1)
+print("2<1",2<1)
+
+a = [1,2,3,4]
+while a:
+    b = a.pop()
+    print(b)
+
+if  [1,2,3]:
+    print("참")
+else:
+    print("거짓")
+
+print("불연산")
+print(bool('python'))
+print(bool(''))
+
+print(bool([1,2,3]))
+
+print(bool([]))
+
+print(bool(0))
+
+print(bool(3))
+
+
