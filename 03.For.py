1c79a7acb8558f92b9f2fc93da31a62ab937d337
seongjin <tjdwns95^^>
2022-04-19 23:08:04 +0900

    add2

--- 03.For.py
+++ 03.For.py
@@ -2,12 +2,24 @@
 
 print("FOR문\n")
 
-print("전형적인 for 문\n")
+print("1. 전형적인 for 문\n")
 test_list = ['one', 'two', 'three']
 for i in test_list:
     print(i)
 
-print("다양한 for문의 사용 \n")
+print("2. 다양한 for문의 사용 \n")
+
+a = [(1,2), (3,4), (5,6)]
+for(first, last) in a:
+    print(first + last)
+
+print("for 문의 응용\n")
+
+
+print("3. for문의 응용\n")
+
+marks = [90, 25, 67, 45, 80]
+
 
 
 
