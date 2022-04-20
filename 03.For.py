79b3836d3fca2f838f49d2ff052990ac3f5d186f
seongjin <tjdwns95^^>
2022-04-20 22:36:25 +0900

    add

--- 03.For.py
+++ 03.For.py
@@ -20,6 +20,24 @@ print("3. for문의 응용\n")
 
 marks = [90, 25, 67, 45, 80]
 
+print("for문과 함께 자주 사용하는 range 함수")
+print("range 함수는 숫자 리스트를 자동으로 만들어 준다")
+print("range(시작숫자, 끝숫자)   끝숫자는 포함되지 않는다")
+a = range(10)
+print(a)
+
+a = range(1,11)
+print(a)
+
+print("range함수의 예시 살펴보기")
+add = 0
+for i in range(1,11):
+    add = add +i
+
+print(add)
+
+
+
 
 
 
