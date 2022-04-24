48dfd68fa55757b02f8c9ee97ff32d0579f264cc
seongjin <tjdwns95^^>
2022-04-24 14:01:11 +0900

    add1

---
+++ 04.File.py
@@ -0,0 +1,20 @@
+f = open("새파일.txt",'w')
+f.close()
+
+print("C:\Users\seongjin\PycharmProjects\Python-Practice 경로에 파일이 생성된게 보여진다")
+
+print("파일 열기 모드 r :  - 읽기모드 파일을 읽기만 할때 사용")
+
+print("파일 열기 모드 w :  - 쓰기모드 파일에 내용을 쓸때 사용")
+
+print("파일 열기 모드 a :  - 추가모드 파일의 마지막에 새로운 내용을 추가할때 사용")
+
+print("파일 경로를 지정해서 생성하고 싶은 경우")
+f = open("C:\doit\새파일.txt",'w')
+f.close()
+
+
+
+
+
+
