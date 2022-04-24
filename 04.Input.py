359e2dfe8413a4ad483e5f9f54c6ff985176c5c4
seongjin <tjdwns95^^>
2022-04-24 13:44:25 +0900

    add2

---
+++ 04.Input.py
@@ -0,0 +1,21 @@
+print("사용자가 입력")
+
+a = input()
+
+print(a)
+
+
+number = input("숫자를 입력하세요")
+print(number)
+
+print("문자열 띄어쓰기는 , 로 한다")
+print("Life", "is", "too","short")
+
+print("한 줄에 결괏값 출력하기")
+for i in range(10):
+    print(i, end = ' ')
+
+
+
+
+
