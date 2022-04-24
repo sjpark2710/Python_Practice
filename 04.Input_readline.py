0138afa68015ebac393a90b195433574a005836a
seongjin <tjdwns95^^>
2022-04-24 14:37:06 +0900

    add4

--- 04.Input_readline.py
+++ 04.Input_readline.py
@@ -1,5 +1,6 @@
 f = open("C:\\Users\\seongjin\\PycharmProjects\\Python-Practice\\새파일.txt",'r')
-line = f.readline()
+
+line = f.readLine()
 print(line)
 f.close()
 
--- 04.Input_readlines.py
+++ 04.Input_readlines.py
@@ -1,9 +1,15 @@
+print('readlines 함수 사용하기')
+
 f = open("C:\\Users\\seongjin\\PycharmProjects\\Python-Practice\\새파일.txt",'r')
-while True:
-    line = f.readline()
-    if not line:break
+lines = f.readlines()
+for line in lines():
     print(line)
 f.close()
+
+
+
+
+
 
 
 
