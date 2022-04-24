47a620693ad5af0caa8682d5639ce11d5408914d
seongjin <tjdwns95^^>
2022-04-24 13:32:19 +0900

    add1

--- 04.Function_lambda.py
+++ 04.Function_lambda.py
@@ -1,1 +1,14 @@
-print("")
+print("lambda 매개변수1, 매개변수2, 매개변수3,.... 매개변수를 사용한 표현식")
+
+add = lambda a, b :a+b
+result = add(3,4)
+print(result)
+
+
+print("def 함수 표현식")
+def add(a,b):
+    return a+b
+
+result = add(3,4)
+print(result)
+
